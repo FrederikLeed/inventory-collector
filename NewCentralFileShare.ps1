4a790ef10a32a0f594aea66f61ef1f3240815ffb
Frederik Leed <leed@intertech.dk>
2023-12-07 12:09:25 +0100

    fix new share settings

--- NewCentralFileShare.ps1
+++ NewCentralFileShare.ps1
@@ -1,10 +1,10 @@
-# PowerShell Script to Create a File Share with List, Write, and Check File Presence Access for Domain Computers
-
-# Define the folder path and share name
-$FolderPath = "C:\SharedFolder"
-$ShareName = "SharedData"
+# Define the folder path, share name, and subfolder path
+$FolderPath = "F:\InventoryData"
+$SubFolderPath = "F:\InventoryData\script"  # Define the subfolder path
+$ShareName = "Inventory"
 $Domain = Get-ADDomain
 $DomainComputers = ($Domain.NetBIOSName) + "\Domain Computers"
+$Everyone = "Everyone"
 
 # Create the folder if it doesn't exist
 if (!(Test-Path -Path $FolderPath)) {
@@ -14,27 +14,57 @@ if (!(Test-Path -Path $FolderPath)) {
     Write-Host "Folder already exists at $FolderPath"
 }
 
+# Create the subfolder if it doesn't exist
+if (!(Test-Path -Path $SubFolderPath)) {
+    New-Item -Path $SubFolderPath -ItemType Directory
+    Write-Host "Subfolder created at $SubFolderPath"
+} else {
+    Write-Host "Subfolder already exists at $SubFolderPath"
+}
+
 # Share the folder with 'Change' permission which includes write access
 $SMBShareParams = @{
     Name        = $ShareName
     Path        = $FolderPath
     Description = "Shared folder for domain computers with specific access"
-    ChangeAccess = $DomainComputers
+    ChangeAccess = $Everyone
 }
 New-SmbShare @SMBShareParams
 Write-Host "Folder shared as $ShareName"
 
-# Set NTFS permissions to allow listing, writing, and checking file presence but not reading file content
-$ACL = Get-Acl $FolderPath
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule(
-    $DomainComputers, 
-    "Write, CreateFiles, CreateDirectories, Delete, ReadAttributes, ReadPermissions, ListDirectory, Synchronize", 
-    "ContainerInherit, ObjectInherit", 
-    "None", 
-    "Allow"
-)
-$ACL.AddAccessRule($AccessRule)
-Set-Acl -Path $FolderPath -AclObject $ACL
-Write-Host "NTFS permissions set for domain computers with specific access requirements"
+# Function to set NTFS permissions
+function Set-NTFSPermissions {
+    param (
+        [string]$Path,
+        [string]$IdentityReference,
+        [string]$FileSystemRights,
+        [bool]$IsInheritanceEnabled = $true
+    )
+
+    $ACL = Get-Acl $Path
+    $ACL.SetAccessRuleProtection(!$IsInheritanceEnabled, $false)  # Break or keep inheritance
+
+    $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule(
+        $IdentityReference, 
+        $FileSystemRights, 
+        "ContainerInherit, ObjectInherit", 
+        "None", 
+        "Allow"
+    )
+    $ACL.AddAccessRule($AccessRule)
+
+    Set-Acl -Path $Path -AclObject $ACL
+    Write-Host "NTFS permissions set for $IdentityReference on $Path"
+}
+
+# Set NTFS permissions for the main folder and the subfolder
+Set-NTFSPermissions -Path $FolderPath -IdentityReference $DomainComputers -FileSystemRights "Write, CreateFiles, CreateDirectories, Delete, ReadAttributes, ReadPermissions, ListDirectory, Synchronize" -IsInheritanceEnabled $false
+Set-NTFSPermissions -Path $SubFolderPath -IdentityReference $DomainComputers -FileSystemRights "ReadAndExecute, ReadAttributes, ReadPermissions, ListDirectory, Synchronize" -IsInheritanceEnabled $false
+
+# Set full control for Administrators and SYSTEM on both folders
+Set-NTFSPermissions -Path $FolderPath -IdentityReference "Administrators" -FileSystemRights "FullControl" -IsInheritanceEnabled $false
+Set-NTFSPermissions -Path $SubFolderPath -IdentityReference "Administrators" -FileSystemRights "FullControl" -IsInheritanceEnabled $false
+Set-NTFSPermissions -Path $FolderPath -IdentityReference "SYSTEM" -FileSystemRights "FullControl" -IsInheritanceEnabled $false
+Set-NTFSPermissions -Path $SubFolderPath -IdentityReference "SYSTEM" -FileSystemRights "FullControl" -IsInheritanceEnabled $false
 
 Write-Host "File share setup complete"
